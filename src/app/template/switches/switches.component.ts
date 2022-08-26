8248a7257de7ee606586a1c49eb8ea596eb02dfe
Danny Armijos <71116392+dmarmijosa@users.noreply.github.com>
2022-08-26 21:20:19 +0200

    fin de formularios por template

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SwitchesComponent {
+  persona={
+    genero:'F',
+    notificaciones:true,
   }
+  terminosycondiciones:boolean=false;
+
 
 }
