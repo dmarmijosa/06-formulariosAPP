467b19181cc39c6b4e4d6c98f7784ee273c4f353
Danny Armijos <71116392+dmarmijosa@users.noreply.github.com>
2022-08-28 18:53:19 +0200

    switches forms

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -7,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SwitchesComponent implements OnInit {
+  miFormulario:FormGroup=this.fb.group({
+    genero          :['M',Validators.required],
+    notificaciones  :[true,Validators.required],
+    terminos        :[false,Validators.requiredTrue]
+  });
+  persona={
+    genero:'F',
+    notificaciones:false
+  }
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      ...this.persona,
+      terminos:true
+    });
+
+/*     this.miFormulario.get('terminos')?.valueChanges.subscribe(resp=>{
+      console.log(resp);
+    })
+ */
+    //REACTIVE EXTENCION RXJS
+   /*  this.miFormulario.valueChanges.subscribe(form=>{
+      delete form.terminos;
+      this.persona=form;
+    }) */
+    this.miFormulario.valueChanges.subscribe(({terminos,...restoArgumentos})=>{
+      this.persona=restoArgumentos;
+    })
+  }
+
+  guardar(){
+    const formValue= {...this.miFormulario.value};
+    delete formValue.terminos;
+    this.persona=formValue;
   }
 
 }
